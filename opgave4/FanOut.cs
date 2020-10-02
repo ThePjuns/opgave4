3f82c35a72a5158c6671db6b4e66ef6b0bbac4ea
Stefan de Haas Hansen <stef9239@edu.easj.dk>
2020-10-02 11:19:19 +0200

    Add project files.

---
+++ opgave4/FanOut.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace opgave4
+{
+    public class FanOut
+    {
+        public int Id { get; set; }
+        private string _name;
+        private double _temp;
+        private double _fugt;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length >= 2)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Not Valid");
+                }
+            }
+        }
+
+        public double Temp
+        {
+            get { return _temp; }
+            set
+            {
+                if (value < 15)
+                {
+                    throw new System.ArgumentException("Temp er for lav");
+                }
+                else if (value > 25)
+                {
+                    throw new System.ArgumentException("Temp er for høj");
+                }
+                else
+                {
+                    _temp = value;
+                }
+            }
+        }
+
+        public double Fugt
+        {
+            get { return _fugt; }
+            set
+            {
+                if (value < 30)
+                {
+                    throw new System.ArgumentException("For lidt fugt");
+                }
+                else if (value > 80)
+                {
+                    throw new System.ArgumentException("For meget fugt");
+                }
+                else
+                {
+                    _fugt = value;
+                }
+            }
+        }
+
+        public FanOut()
+        {
+
+        }
+
+        public FanOut(int id, string name, double temp, double fugt)
+        {
+            Id = id;
+            Name = name;
+            Temp = temp;
+            Fugt = fugt;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Temp: {Temp}, Fugt: {Fugt}";
+        }
+    }
+}
+
---
+++ rest/Controllers/FanController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using opgave4;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace rest.Controllers
+{
+    [Route("fanoutput")]
+    [ApiController]
+    public class FanController : ControllerBase
+    {
+
+        private static readonly List<FanOut> fanOuts = new List<FanOut>()
+        {
+            new FanOut(1, "stue", 24.3, 65.02),
+            new FanOut(2, "værelseet", 24.2, 52.53),
+            new FanOut(3, "bryggers", 24.3, 79.02),
+            new FanOut(4, "toilet", 15.6, 50.23),
+            new FanOut(5, "værelseto", 24.3, 79.02)
+        };
+
+        // GET: api/<FanController>
+        [HttpGet]
+        public IEnumerable<FanOut> Get()
+        {
+            return fanOuts;
+        }
+
+        // GET api/<FanController>/5
+        [HttpGet("{id}")]
+        public FanOut Get(int id)
+        {
+            return fanOuts.Find(i => i.Id == id);
+        }
+
+        // POST api/<FanController>
+        [HttpPost]
+        public void Post([FromBody] FanOut value)
+        {
+            fanOuts.Add(value);
+        }
+
+        // PUT api/<FanController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] FanOut value)
+        {
+            FanOut fanOut = Get(id);
+            if (fanOut != null)
+            {
+                fanOut.Name = value.Name;
+                fanOut.Temp = value.Temp;
+                fanOut.Fugt = value.Fugt;
+            }
+        }
+
+        // DELETE api/<FanController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            FanOut fanOut = Get(id);
+            fanOuts.Remove(fanOut);
+        }
+    }
+}
